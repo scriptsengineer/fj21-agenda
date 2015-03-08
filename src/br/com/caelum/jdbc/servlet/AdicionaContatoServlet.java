9e41636bbfdf355548f8448ab2a2981a9bbac25f
Rafael Correa <rafinha2108@gmail.com>
2015-03-08 20:59:38 -0300

    Removido código html para envio para uma página JSP.

--- src/br/com/caelum/jdbc/servlet/AdicionaContatoServlet.java
+++ src/br/com/caelum/jdbc/servlet/AdicionaContatoServlet.java
@@ -7,6 +7,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -51,10 +52,7 @@ public class AdicionaContatoServlet extends HttpServlet{
 		dao.adicionaContato(contato);
 		
 		//Imprime frase de que contato foi adicionado
-		out.println("<html>");
-		out.println("<body>");
-		out.println("Contato "+nome+"salvo com sucesso!");
-		out.println("</body>");
-		out.println("</html>");
+		RequestDispatcher rd = request.getRequestDispatcher("/contato-adicionado.jsp");
+		rd.forward(request,response);
 	}
 }
