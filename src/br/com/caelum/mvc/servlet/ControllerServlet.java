59ba3b5cd4539067124a05844011c2650882900f
Rafael Correa <rafinha2108@gmail.com>
2015-03-10 12:31:42 -0300

    Adicão da anotação de servlet para "/mvc".

--- src/br/com/caelum/mvc/servlet/ControllerServlet.java
+++ src/br/com/caelum/mvc/servlet/ControllerServlet.java
@@ -3,6 +3,7 @@ package br.com.caelum.mvc.servlet;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,6 +15,7 @@ import br.com.caelum.mvc.logica.Logica;
  * @author Rafael
  *
  */
+@WebServlet("/mvc")
 public class ControllerServlet extends HttpServlet{
 
 	@Override
